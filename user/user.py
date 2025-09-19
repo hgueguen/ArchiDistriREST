bf9dca5dbf7bed3f04dc2d63d2964979d03cd5a8
hgueguen <hortense.gueguen@etudiant.univ-lr.fr>
2025-09-19 16:44:31 +0200

    User fait

--- user/user.py
+++ user/user.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, request, jsonify, make_response
-import requests
+from flask import Flask, request, jsonify, make_response
 import json
 from werkzeug.exceptions import NotFound
 
@@ -11,10 +10,61 @@ HOST = '0.0.0.0'
 with open('{}/databases/users.json'.format("."), "r") as jsf:
    users = json.load(jsf)["users"]
 
+def write(users):
+   with open('{}/databases/users.json'.format("."), 'w') as f:
+      full = {}
+      full['users']=users
+      json.dump(full, f)
+
 @app.route("/", methods=['GET'])
 def home():
    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
 
+@app.route("/users", methods=['GET'])
+def get_users():
+   res = make_response(jsonify(users), 200)
+   return res
+
+@app.route("/users/<userid>", methods=['GET'])
+def get_user_byid(userid):
+   for user in users:
+      if str(user["id"]) == str(userid):
+         res = make_response(jsonify(user),200)
+         return res
+   return make_response(jsonify({"error":"User ID not found"}),500)
+
+@app.route("/users/<userid>", methods=['POST'])
+def add_user(userid):
+   req = request.get_json()
+
+   for user in users:
+      if str(user["id"]) == str(userid):
+         print(user["id"])
+         return make_response(jsonify({"error":"User ID already exists"}),500)
+
+   users.append(req)
+   write(users)
+   res = make_response(jsonify(req),200)
+   return res
+
+@app.route("/users/<userid>", methods=['DELETE'])
+def delete_user(userid):
+   for user in users:
+      if str(user["id"]) == str(userid):
+         users.remove(user)
+         write(users)
+         return make_response(jsonify({"message":"User ID {} deleted".format(userid)}),200)
+   return make_response(jsonify({"error":"User ID not found"}),500)
+
+@app.route("/users/<userid>", methods=['PUT'])
+def update_user(userid):
+   req = request.get_json()
+   for user in users:
+      if str(user["id"]) == str(userid):
+         user.update(req)
+         write(users)
+         return make_response(jsonify(user),200)
+   return make_response(jsonify({"error":"User ID not found"}),500)
 
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
